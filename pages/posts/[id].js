825b01d6e2b63ab9651362be1d1e1dc8fc14dbf7
RyomaSeki <seki.r621@gmail.com>
2022-04-24 21:52:17 +0900

    first

---
+++ pages/posts/[id].js
@@ -0,0 +1,36 @@
+import Layout from "../../components/layout"
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
+export async function getStaticProps({ params }) {
+    const postData = getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </Layout>
+    )
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
